46139eecc48eacd42cd2a401b1ab621b101ab267
Dantas58 <pedrodantas8@gmail.com>
2021-03-26 08:58:48 +0000

    Documentaçao das funçoes

--- parser.c
+++ parser.c
@@ -1,3 +1,7 @@
+/**
+ * @file Ficheiro no qual se encontram as funções referentes ao parser.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,9 +15,14 @@ int count = -1;
 
 
 
-
-
-
+/**
+ * \brief Função que fará o parse de uma linha.
+ *
+ * Esta função faz o parse de uma dada linha e, dependendo do input, realiza diferentes operações.
+ *
+ * @param line Linha a que faremos parse.
+ *
+ */
 void parse(char *line) {
 
     char *delims = " \t\n";
@@ -92,20 +101,35 @@ void parse(char *line) {
     print(count);
 }
 
-
+/**
+ * \brief Função auxiliar que nos permite adicionar um elemento à stack.
+ *
+ *
+ * @param x É o elemento que será adicionado à stack.
+ */
 void push(int x) {
     count++;
     stack[count] = x;
 }
 
-
+/**
+ * \brief Função auxiliar que nos permite retirar o último elemento da stack.
+ *
+ *
+ * @returns res Último elemento da stack.
+ */
 int pop() {
     int res = stack[count];
     count--;
     return res;
 }
 
-
+/**
+ * \brief Função utilizada para imprimir a stack.
+ *
+ *
+ * @param x É o tamanho da stack a ser imprimida.
+ */
 void print(int x) {
     int y = 0;
     while (y<=x) {
