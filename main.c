5bf8d021cdf9ec800a390d075bb228c5f6d4447e
Dantas58 <pedrodantas8@gmail.com>
2021-03-26 08:58:19 +0000

    Documentaçao das funçoes

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
+/**
+ * @file Ficheiro no qual se encontra o programa principal.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
 #include "parser.h"
+
+/**
+ * \brief Função principal do programa.
+ *
+ *
+ *
+ * @returns 0 valor 0
+ */
 
 int main() {
     char line[10240];
