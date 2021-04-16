4c6d3c02f51779014aa3c4d9958d76511e30f42d
jotaalvim <joaoafonsoalvim@gmail.com>
2021-04-16 08:18:44 +0100

    finalização

--- parser.c
+++ parser.c
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include <string.h>
 #include <math.h>
+#include <stdio.h>
 
 void soma(STACK *s) {
     DATA p = pop(s);
@@ -32,19 +33,19 @@ void sub(STACK *s){
     DATA sg = pop(s);
     TYPE ts = sg.type;
     if (tp==LONG&&ts==LONG){DATA z;
-        make_datas(z,LONG,p.elems.LONG-sg.elems.LONG);
+        make_datas(z,LONG,-p.elems.LONG+sg.elems.LONG);
         push(s,z);}
     else if (tp==DOUBLE&&ts==LONG){
         DATA z;
-        make_datas(z,DOUBLE,p.elems.DOUBLE-sg.elems.LONG);
+        make_datas(z,DOUBLE,-p.elems.DOUBLE+sg.elems.LONG);
         push(s,z);}
     else if (tp == LONG && ts == DOUBLE){
         DATA z;
-        make_datas(z,DOUBLE,p.elems.LONG-sg.elems.DOUBLE);
+        make_datas(z,DOUBLE,-p.elems.LONG+sg.elems.DOUBLE);
         push(s,z);}
     else if (tp==DOUBLE&&ts==DOUBLE){
         DATA z;
-        make_datas(z,DOUBLE,p.elems.DOUBLE-sg.elems.DOUBLE);
+        make_datas(z,DOUBLE,-p.elems.DOUBLE+sg.elems.DOUBLE);
         push(s,z);}
 } 
 
@@ -55,19 +56,19 @@ void divi(STACK *s) {
     TYPE ts = sg.type;
     if (tp == LONG && ts == LONG){
         DATA z;
-        make_datas(z, LONG, p.elems.LONG/sg.elems.LONG);
+        make_datas(z, LONG, sg.elems.LONG/p.elems.LONG);
         push(s,z);}
     else if (tp == DOUBLE && ts == LONG){
         DATA z;
-        make_datas(z, DOUBLE, p.elems.DOUBLE/sg.elems.LONG);
+        make_datas(z, DOUBLE, sg.elems.LONG/p.elems.DOUBLE);
         push(s,z);}
     else if (tp==LONG&&ts==DOUBLE){
         DATA z;
-        make_datas(z, DOUBLE, p.elems.LONG/sg.elems.DOUBLE);
+        make_datas(z, DOUBLE, sg.elems.DOUBLE/p.elems.LONG);
         push(s,z);}
     else if (tp == DOUBLE && ts == DOUBLE){
         DATA z;
-        make_datas(z, DOUBLE, p.elems.DOUBLE/sg.elems.DOUBLE);
+        make_datas(z, DOUBLE, sg.elems.DOUBLE/p.elems.DOUBLE);
         push(s,z);}
 } 
 void mult(STACK *s) {
@@ -134,7 +135,7 @@ void mod(STACK *s) { // FIXME NÂO FUNCIONA
     push(s,k);
 }
 
-void expo(STACK *s) {
+void expo (STACK *s) {
     DATA p = pop(s);
     TYPE tp = p.type;
     DATA sg = pop(s);
@@ -188,25 +189,120 @@ void not(STACK *s) {
 }
 
 void arr(STACK *s) {
-    DATA p = pop(s);
-    push(s,z);
-    DATA p = pop(s);
-    DATA z;
+    DATA a = pop(s);
+    DATA b = pop(s);
+    DATA c = pop(s);
+    push(s,b);
+    push(s,a);
+    push(s,c);
+}
 
-    push(s,z);
-    DATA p = pop(s);
-    DATA z;
+void stop(STACK *s) {
+    DATA a = pop(s);
+    DATA b = pop(s);
+    push(s,a);
+    push(s,b);
+}
 
-    push(s,z);
-    DATA p = pop(s);
-    DATA z;
+void und(STACK *s) {
+    DATA a = pop(s);
+    push(s,a);
+    push(s,a);
+}
 
-    push(s,z);
-    DATA p = pop(s);
-    DATA z;
 
+//void tpindex (STACK *s) {
+//    DATA a = pop(s);
+//    TYPE ta = a.type;
+//    if (ta == LONG) {
+//        DATA z;
+//        make_datas(z,LONG,s->p.elems.LONG);
+//        //s->p[(s->size)-1-v_n.l].valor.l
+//        push(s,z);
+//    } else
+//    if (ta == DOUBLE) {
+//        
+//    } else
+//    if (ta == CHAR) {
+//        
+//    } else 
+//    if (ta == STRING) {
+//        
+//    }
+//}
+
+void lei (STACK*s){
+    char x[256];
+    assert(scanf("\n%100[^\n]", x) == 1);
+    DATA z;
+    make_datas(z,STRING,x)
     push(s,z);
 }
+
+void trsc (STACK*s){
+    DATA a = pop(s);
+    TYPE ta= a.type;
+    if (ta == LONG) {
+        DATA z;
+        make_datas(z,CHAR,(char)a.elems.LONG);
+        push(s,z);}
+    else if (ta == DOUBLE) {
+        DATA k;
+        make_datas(k,CHAR,(char) a.elems.DOUBLE);
+        push(s,k);}
+    else if (ta == CHAR) {
+        DATA q;
+        make_datas(q,CHAR,a.elems.CHAR);
+        push(s,q);
+    }
+}
+
+void trsi (STACK*s){
+    DATA a = pop(s);
+    TYPE ta= a.type;
+    if (ta == LONG) {
+        DATA z;
+        make_datas(z,LONG,a.elems.LONG);
+        push(s,z);}
+    else if (ta == DOUBLE) {
+        DATA k;
+        make_datas(k,LONG,(int) a.elems.DOUBLE);
+        push(s,k);}
+    else if (ta == CHAR) {
+        DATA q;
+        make_datas(q,LONG,(int)a.elems.CHAR);
+        push(s,q);
+    }
+    else if (ta == STRING) {
+        DATA q;
+        make_datas(q,LONG,atoi(a.elems.STRING));
+        push(s,q);
+    }
+}
+
+void trsd (STACK*s){
+    DATA a = pop(s);
+    TYPE ta= a.type;
+    if (ta == LONG) {
+        DATA z;
+        make_datas(z,DOUBLE,(double)a.elems.LONG);
+        push(s,z);}
+    else if (ta == DOUBLE) {
+        DATA k;
+        make_datas(k,DOUBLE,a.elems.DOUBLE);
+        push(s,k);}
+    else if (ta == CHAR) {
+        DATA q;
+        make_datas(q,DOUBLE,(double)a.elems.CHAR);
+        push(s,q);
+    }
+    else if (ta == STRING) {
+        DATA q;
+        make_datas(q,DOUBLE,atof(a.elems.STRING));
+        push(s,q);
+    }
+}
+
 void parse(char *line, STACK *string) {
     char *delims = " \t\n";
     for(char *token = strtok(line, delims); token != NULL; token = strtok(NULL, delims)) {
@@ -256,22 +352,23 @@ void parse(char *line, STACK *string) {
                     not(string); break;
                 case '@' :
                     arr(string); break;
-                //case ';' : 
-                //    pop(s); break;
-                //case '_' :
-                //    und(s); break;
-                //case '\\' :
-                //    switchtop(s); break;
+                case '\\' :
+                    stop(string); break;
+                case ';' :
+                    pop(string); break;
+                case '_' :
+                    und(string); break;
                 //case '$' :
-                //    tpindex(s); break;
-                //case 'l' :
-                //    leitura(s); break;
-                //case 'c' : 
-                //    trschar(s); break;
-                //case 'i' :
-                //    trsint(s); break;
-                //case 'f' :
-                //    trsdouble(s); break;
+                //    tpindex(string); break;
+                
+                case 'l' :
+                    lei(string); break;
+                case 'c' :
+                    trsc(string); break;
+                case 'i' :
+                    trsi(string); break;
+                case 'f' :
+                    trsd(string); break;
                 //default : break;
             }
     }
--- parser.h
+++ parser.h
@@ -7,8 +7,82 @@
 
 #include "stack.h"
 
-
+/**
+ * \brief   Função que soma os 2 elementos do topo da Stack.
+ * @param s Stack
+ */
 void soma(STACK *s);
+
+/**
+ * \brief   Função que subtrai o elemento do topo da Stack ao elemento a seguir.
+ * @param s Stack
+ */
+void sub(STACK *s);
+
+/**
+ * \brief   Função que permite multiplicar os 2 primeiros elementos do topo da Stack.
+ * @param s Stack
+ */
+void mult(STACK *s);
+
+/**
+ * \brief   Função que divide o 2º elemento do topo da Stack pelo elemento do topo da Stack.
+ * @param s Stack
+ */
+void divi(STACK *s);
+
+/**
+ * \brief   Função que decrementa 1 valor ao elemento do topo da Stack.
+ * @param s Stack
+ */
+
+void parA(STACK *s);
+
+/**
+ * \brief   Função que incrementa 1 valor ao elemento do topo da Stack.
+ * @param s Stack
+ */
+void parF(STACK *s);
+
+/**
+ * \brief   Função do resto da divisão inteira do 2º elemento pelo 1º elemento do topo da Stack.
+ * @param s Stack
+ */
+void mod(STACK *s);
+
+/**
+ * \brief   Função que calcula a potência que tem como base o elemento do topo da Stack e como expoente o elemento a seguir.
+ * @param s Stack
+ */
+void expo (STACK *s);
+
+/**
+ * \brief   Operador bitwise onde cada bit é comparado e é retornado o valor 1 quando ambos os bits forem 1 e 0 caso contrário.
+ *
+
+ * @param s Stack
+ */
+void con(STACK *s);
+
+/**
+ * \brief   Operador bitwise onde cada bit é comparado e é retornado o valor 1 quando um dos bits for 1 e 0 caso contrário.
+ *
+
+ * @param s Stack
+ */ 
+void dis(STACK *s);
+
+/**
+ * \brief   Operador bitwise onde cada bit é comparado e é retornado o valor 1 quando os bits comparados forem diferentes e 0 caso contrário.
+ * @param s Stack
+ */
+void xor(STACK *s);
+
+/**
+ * \brief   Operador bitwise onde cada bit é comparado que retorna o valor representado em Complemento2.
+ * @param s Stack
+ */
+void not (STACK *s);
 
 /**
  * \brief      Esta função divide a string dada no input usando delimitadores, separando os números dos operadores.
@@ -16,8 +90,25 @@ void soma(STACK *s);
  * @param line A linha que foi lida e à qual se irá aplicar o parse
  * @param s    Stack
  */
+
+void trsc (STACK*s);
+
+void arr(STACK *s);
+
+void stop(STACK *s);
+
+void und(STACK *s);
+
+void lei (STACK*s);
+
+void trsc (STACK*s);
+
+void trsi (STACK*s);
+
+void trsd (STACK*s);
+
 void parse(char *line, STACK *s);
 
-//void pushaux (struct Stack *s, elemtype x, elemvalor y);
+	//void pushaux (STACK *s, elemtype x, elemvalor y);
 
 #endif
