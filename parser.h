a593dfc8aac539d9a14e9b96caace2da8693aff4
Dantas58 <pedrodantas8@gmail.com>
2021-05-01 23:36:17 +0100

    doc

--- parser.h
+++ parser.h
@@ -13,35 +13,11 @@
 
 #include "stack.h"
 
-/**
- * \brief   Função que soma os 2 elementos do topo da Stack.
- * @param s Stack
- */
-//void soma(STACK *s);
-
-/**
- * \brief   Função que subtrai o elemento do topo da Stack ao elemento a seguir.
- * @param s Stack
- */
-//void sub(STACK *s);
-
-/**
- * \brief   Função que permite multiplicar os 2 primeiros elementos do topo da Stack.
- * @param s Stack
- */
-//void mult(STACK *s);
-
-/**s
- * \brief   Função que divide o 2º elemento do topo da Stack pelo elemento do topo da Stack.
- * @param s Stack
- */
-//void divi(STACK *s);
 
 /**
  * \brief   Função que decrementa 1 valor ao elemento do topo da Stack.
  * @param s Stack
  */
-
 void parA(STACK *s);
 
 /**
@@ -51,44 +27,65 @@ void parA(STACK *s);
 void parF(STACK *s);
 
 /**
- * \brief   Função do resto da divisão inteira do 2º elemento pelo 1º elemento do topo da Stack.
- * @param s Stack
- */
-//void mod(STACK *s);
-
-/**
- * \brief   Função que calcula a potência que tem como base o elemento do topo da Stack e como expoente o elemento a seguir.
- * @param s Stack
- */
-//void expo (STACK *s);
-
-/**
- * \brief   Operador bitwise onde cada bit é comparado e é retornado o valor 1 quando ambos os bits forem 1 e 0 caso contrário.
- *
-
- * @param s Stack
- */
-//void con(STACK *s);
-
-/**
- * \brief   Operador bitwise onde cada bit é comparado e é retornado o valor 1 quando um dos bits for 1 e 0 caso contrário.
- *
-
- * @param s Stack
- */ 
-//void dis(STACK *s);
-
-/**
- * \brief   Operador bitwise onde cada bit é comparado e é retornado o valor 1 quando os bits comparados forem diferentes e 0 caso contrário.
- * @param s Stack
- */
-//void xor(STACK *s);
-
-/**
  * \brief   Operador bitwise onde cada bit é comparado que retorna o valor representado em Complemento2.
  * @param s Stack
  */
 void not (STACK *s);
+
+/**
+* \brief Função que troca os três elementos do topo da Stack entre si.
+* @param s Stack
+*/
+void arr(STACK*s);
+
+/**
+ * \brief Função que troca os dois elementos do topo da Stack.
+ * @param s Stack
+ */
+void stop(STACK*s);
+
+
+/**
+ * \brief Função que duplica o elemento do topo da Stack.
+ * @param s Stack
+ */
+void und(STACK*s);
+
+
+/**
+* \brief Função que lê uma linha.
+* @param s Stack
+*/
+void lei (STACK*s);
+
+
+/**
+ * \brief Função que copia o n-ésimo elemento (definido pelo elemento que se encontra no topo da stack) para o topo da Stack.
+ * @param s Stack
+ */
+void tpi (STACK*s);
+
+
+/**
+ * \brief Função que copia o n-ésimo elemento (definido pelo elemento que se encontra no topo da stack) para o topo da Stack.
+ * @param s Stack
+ */
+void trsc (STACK*s);
+
+
+/**
+* \brief Função que converte o elemento no topo da Stack num Inteiro.
+* @param s Stack 
+*/
+void trsi (STACK*s);
+
+
+/**
+* \brief Função que converte o elemento no topo da Stack num Double.
+* @param s Stack 
+*/
+void trsd (STACK*s);
+
 
 /**
  * \brief      Esta função divide a string dada no input usando delimitadores, separando os números dos operadores.
@@ -96,25 +93,6 @@ void not (STACK *s);
  * @param line A linha que foi lida e à qual se irá aplicar o parse
  * @param s    Stack
  */
-
-void trsc (STACK*s);
-
-void arr(STACK*s);
-
-void stop(STACK*s);
-
-void und(STACK*s);
-
-void lei (STACK*s);
-
-void tpi (STACK*s);
-
-void trsc (STACK*s);
-
-void trsi (STACK*s);
-
-void trsd (STACK*s);
-
 void parse(char *line, STACK *s);
 
 	//void pushaux (STACK *s, elemtype x, elemvalor y);
